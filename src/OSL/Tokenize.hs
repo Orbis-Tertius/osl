0e2e1744e531ef18105242dbbd36ca511ee85121
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-08-03 18:23:38 -0400

    whitespace parsing

--- src/OSL/Tokenize.hs
+++ src/OSL/Tokenize.hs
@@ -1,10 +1,11 @@
 module OSL.Tokenize (tokenize) where
 
 
+import Control.Monad (void)
 import Data.Either.Combinators (mapLeft)
 import Data.Text (Text, pack)
-import Text.Parsec (SourceName, SourcePos)
-import Text.Parsec.Prim (parse)
+import Text.Parsec (SourceName, SourcePos, getPosition, eof, oneOf, (<|>), try, string, noneOf, anyChar)
+import Text.Parsec.Prim (parse, many)
 import Text.Parsec.Text (Parser)
 
 import OSL.Types.ErrorMessage (ErrorMessage (..))
@@ -16,7 +17,42 @@ tokenize name = mapLeft (ErrorMessage () . pack . show) . parse tokens name
 
 
 tokens :: Parser [(Token, SourcePos)]
-tokens = todo
+tokens = do
+  void (many whitespace)
+  ts <- many $ do
+         t <- token
+         p <- getPosition
+         void (many whitespace)
+         return (t, p)
+  eof
+  return ts
+
+
+whitespace :: Parser ()
+whitespace = void (oneOf " \t\r\n") <|> try oneLineComment <|> try multiLineComment
+
+
+oneLineComment :: Parser ()
+oneLineComment = do
+  void $ string "--"
+  void $ many (noneOf lineEnding)
+  void $ oneOf lineEnding
+  where
+    lineEnding = "\r\n"
+
+
+multiLineComment :: Parser ()
+multiLineComment = do
+  void $ string "{-"
+  rest
+
+  where
+    rest :: Parser ()
+    rest = void (try (string "-}")) <|> (void anyChar >> rest)
+
+
+token :: Parser Token
+token = todo
 
 
 todo :: a
