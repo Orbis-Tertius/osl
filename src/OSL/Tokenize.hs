0ce11cc3f70264b18adb2b7222d7de91d5b9c45a
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-08-03 18:20:21 -0400

    tokenize outer body

--- src/OSL/Tokenize.hs
+++ src/OSL/Tokenize.hs
@@ -1,15 +1,22 @@
 module OSL.Tokenize (tokenize) where
 
 
-import Data.Text (Text)
+import Data.Either.Combinators (mapLeft)
+import Data.Text (Text, pack)
 import Text.Parsec (SourceName, SourcePos)
+import Text.Parsec.Prim (parse)
+import Text.Parsec.Text (Parser)
 
 import OSL.Types.ErrorMessage (ErrorMessage (..))
 import OSL.Types.Token (Token)
 
 
-tokenize :: SourceName -> Text -> Either (ErrorMessage SourcePos) [(Token, SourcePos)]
-tokenize = todo
+tokenize :: SourceName -> Text -> Either (ErrorMessage ()) [(Token, SourcePos)]
+tokenize name = mapLeft (ErrorMessage () . pack . show) . parse tokens name
+
+
+tokens :: Parser [(Token, SourcePos)]
+tokens = todo
 
 
 todo :: a
