65e81ed63471bc9121af982a8f24640ad1d54552
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-08-04 08:10:32 -0400

    declaration grammar

--- src/OSL/Parse.hs
+++ src/OSL/Parse.hs
@@ -1,14 +1,20 @@
+{-# LANGUAGE LambdaCase #-}
+
+
 module OSL.Parse (parseContext) where
 
 
+import Control.Monad (guard, mzero)
 import Data.Either.Combinators (mapLeft)
-import Data.Text (pack)
-import Text.Parsec (SourceName, SourcePos, Parsec, many, eof)
+import Data.Text (Text, pack, unpack)
+import Text.Parsec (SourceName, SourcePos, Parsec, many, eof, token, (<|>))
 import qualified Text.Parsec.Prim as Prim
 
 import OSL.Types.ErrorMessage (ErrorMessage (..))
-import OSL.Types.OSL (Context (..), Name, Declaration (..))
+import OSL.Types.OSL (Context (..), Name, Declaration (..), Term (..), Type (..))
+import qualified OSL.Types.Keyword as K
 import OSL.Types.Token (Token (..))
+import qualified OSL.Types.Token as T
 
 
 parseContext :: SourceName -> [(Token, SourcePos)] -> Either (ErrorMessage ()) (Context SourcePos)
@@ -19,7 +25,24 @@ type Parser = Parsec [(Token, SourcePos)] ()
 
 
 parse' :: Parser a -> SourceName -> [(Token, SourcePos)] -> Either (ErrorMessage ()) a
-parse' p name = mapLeft (ErrorMessage () . pack . show) . Prim.parse p name
+parse' p n = mapLeft (ErrorMessage () . pack . show) . Prim.parse p n
+
+
+consume :: ((Token, SourcePos) -> Maybe a) -> Parser a
+consume = token (unpack . printToken . fst) snd
+
+
+consumeExact :: Token -> (SourcePos -> a) -> Parser a
+consumeExact tok tm =
+  consume (\(t, p) -> guard (t == tok) >> return (tm p))
+
+
+consumeExact_ :: Token -> Parser ()
+consumeExact_ tok = consumeExact tok (const ())
+
+
+printToken :: Token -> Text
+printToken = pack . show
 
 
 context :: Parser (Context SourcePos)
@@ -30,7 +53,54 @@ context = do
 
 
 declaration :: Parser (Name, Declaration SourcePos)
-declaration = todo
+declaration = dataDeclaration <|> defDeclaration <|> freeDeclaration
+
+
+dataDeclaration :: Parser (Name, Declaration SourcePos)
+dataDeclaration = do
+  consumeExact_ (T.Keyword K.Data)
+  n <- name
+  consumeExact_ T.Congruent
+  t <- type'
+  consumeExact_ T.Period
+  return (n, Data t)
+
+
+defDeclaration :: Parser (Name, Declaration SourcePos)
+defDeclaration = do
+  consumeExact_ (T.Keyword K.Def)
+  n <- name
+  consumeExact_ T.Colon
+  ty <- type'
+  consumeExact_ T.DefEquals
+  def <- term
+  consumeExact_ T.Period
+  return (n, Defined ty def)
+
+
+freeDeclaration :: Parser (Name, Declaration SourcePos)
+freeDeclaration = do
+  n <- name
+  consumeExact_ T.Colon
+  t <- type'
+  consumeExact_ T.Period
+  return (n, FreeVariable t)
+
+
+name :: Parser Name
+name =
+  consume $
+    \case
+      (T.Var x, _) -> pure x
+      _            -> mzero
+
+
+type' :: Parser (Type SourcePos)
+type' = todo
+
+
+term :: Parser (Term SourcePos)
+term = todo
 
 
 todo :: a
