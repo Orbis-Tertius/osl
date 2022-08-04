b06fa233de7753e6b2ca88a11f588ed2858a967c
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-08-04 07:30:21 -0400

    wip tokenizer

--- src/OSL/Tokenize.hs
+++ src/OSL/Tokenize.hs
@@ -1,16 +1,20 @@
+{-# LANGUAGE LambdaCase #-}
+
+
 module OSL.Tokenize (tokenize) where
 
 
 import Control.Monad (void)
 import Data.Either.Combinators (mapLeft)
 import Data.Text (Text, pack, cons)
-import Text.Parsec (SourceName, SourcePos, getPosition, eof, oneOf, (<|>), try, string, noneOf, anyChar, choice)
+import Text.Parsec (SourceName, SourcePos, getPosition, eof, oneOf, (<|>), try, string, noneOf, anyChar, choice, char, many1)
 import Text.Parsec.Prim (parse, many)
 import Text.Parsec.Text (Parser)
 
 import OSL.Types.ErrorMessage (ErrorMessage (..))
 import OSL.Types.OSL (Name (..))
 import OSL.Types.Keyword (Keyword)
+import qualified OSL.Types.Keyword as K
 import OSL.Types.Token (Token)
 import qualified OSL.Types.Token as T
 
@@ -65,9 +69,9 @@ token =
   , T.Colon <$ string ":"
   , T.OpenParen <$ string "("
   , T.CloseParen <$ string ")"
-  , T.ConstN <$> constantNatural
-  , T.ConstZ <$> constantInteger
-  , T.ConstFin <$> constantFinite
+  , constantNatural
+  , constantInteger
+  , constantFinite
   , T.AddNOp <$ string "+N"
   , T.AddNOp <$ string "+ℕ"
   , T.MulNOp <$ string "×ℕ"
@@ -109,7 +113,47 @@ token =
 
 
 keyword :: Parser Keyword
-keyword = todo
+keyword =
+  choice
+  $
+  try
+  <$>
+  [ K.Type <$ string "Type"
+  , K.Prop <$ string "Prop"
+  , K.N <$ string "ℕ"
+  , K.N <$ string "N"
+  , K.Z <$ string "ℤ"
+  , K.Z <$ string "Z"
+  , K.Fin <$ string "Fin"
+  , K.Cast <$ string "cast"
+  , K.Data <$ string "data"
+  , K.To <$ string "to"
+  , K.From <$ string "from"
+  , K.Def <$ string "def"
+  , K.Maybe <$ string "Maybe"
+  , K.Maybe' <$ string "maybe"
+  , K.Just' <$ string "just"
+  , K.Nothing' <$ string "nothing"
+  , K.Exists <$ string "exists"
+  , K.List <$ string "List"
+  , K.Length <$ string "length"
+  , K.Nth <$ string "nth"
+  , K.Sum <$ string "Σ"
+  , K.Sum <$ string "sum"
+  , K.Pi1 <$ string "π1"
+  , K.Pi1 <$ string "pi1"
+  , K.Pi2 <$ string "π2"
+  , K.Pi2 <$ string "pi2"
+  , K.Iota1 <$ string "ι1"
+  , K.Iota2 <$ string "ι2"
+  , K.Iota1 <$ string "iota1"
+  , K.Iota2 <$ string "iota2"
+  , K.Map <$ string "Map"
+  , K.Lookup <$ string "lookup"
+  , K.Keys <$ string "keys"
+  , K.SumMapLength <$ string "sumMapLength"
+  , K.SumListLookup <$ string "sumListLookup"
+  ]
 
 
 name :: Parser Name
@@ -119,17 +163,61 @@ name = do
   return (Name (cons begin (pack rest)))
 
 
-constantNatural :: Parser Integer
-constantNatural = todo
+constantNatural :: Parser Token
+constantNatural = do
+  i <- nonNegativeIntegerLiteral
+  void $ char 'N' <|> char 'ℕ'
+  return (T.ConstN i)
 
 
-constantInteger :: Parser Integer
-constantInteger = todo
+constantInteger :: Parser Token
+constantInteger = do
+  i <- integerLiteral
+  void $ char 'Z' <|> char 'ℤ'
+  return (T.ConstZ i)
 
 
-constantFinite :: Parser Integer
-constantFinite = todo
+constantFinite :: Parser Token
+constantFinite = do
+  void $ string "fin"
+  whitespace
+  void $ char '('
+  i <- integerLiteral
+  whitespace
+  void $ char ')'
+  return (T.ConstFin i)
 
 
-todo :: a
-todo = todo
+integerLiteral :: Parser Integer
+integerLiteral = negativeIntegerLiteral <|> nonNegativeIntegerLiteral
+
+
+negativeIntegerLiteral :: Parser Integer
+negativeIntegerLiteral = do
+  void (char '-')
+  negate <$> nonNegativeIntegerLiteral
+
+
+nonNegativeIntegerLiteral :: Parser Integer
+nonNegativeIntegerLiteral =
+  digitsToInteger <$> many1 (oneOf ['0'..'9'])
+
+
+digitToInteger :: Char -> Integer
+digitToInteger =
+  \case
+    '0' -> 0
+    '1' -> 1
+    '2' -> 2
+    '3' -> 3
+    '4' -> 4
+    '5' -> 5
+    '6' -> 6
+    '7' -> 7
+    '8' -> 8
+    '9' -> 9
+    _   -> 0
+
+
+digitsToInteger :: String -> Integer
+digitsToInteger = foldl (\a x -> a * 10 + x) 0 . fmap digitToInteger
