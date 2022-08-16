7ad98342aefe85501f4de74d07007445746d25c5
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-08-16 19:14:09 -0400

    wip iota1 translation

--- src/OSL/Translate.hs
+++ src/OSL/Translate.hs
@@ -8,76 +8,103 @@ module OSL.Translate
 
 
 import qualified Data.Map as Map
+import Data.Text (pack)
 
 import OSL.Term (termAnnotation)
 import OSL.Types.ErrorMessage (ErrorMessage (..))
 import qualified OSL.Types.OSL as OSL
 import qualified OSL.Types.Sigma11 as S11
 import OSL.Types.Translation (Translation (Formula, Term, Mapping))
-import OSL.Types.TranslationContext (TranslationContext (..), Mapping (..), LeftMapping (..), RightMapping (..))
+import OSL.Types.TranslationContext (TranslationContext (..), Mapping (..), LeftMapping (..), RightMapping (..), ChoiceMapping (..), ValuesMapping (..))
 
 
 -- Provides a translation for the given term in
 -- the given context.
 translate
   :: TranslationContext ann
+  -> OSL.Type ann
   -> OSL.Term ann
   -> Either (ErrorMessage ann) Translation
-translate ctx@(TranslationContext _ mappings) =
+translate ctx@(TranslationContext _ mappings) termType =
   \case
     OSL.NamedTerm ann name ->
       case Map.lookup name mappings of
         Just (ScalarMapping n) -> return (Term (S11.Var n))
         Just m -> return (Mapping (S11.Var <$> m))
         Nothing -> Left (ErrorMessage ann "un-mapped name")
-    OSL.Apply _ (OSL.Apply _ (OSL.AddN _) a) b ->
-      Term <$> (S11.Add <$> translateToTerm ctx a
-                        <*> translateToTerm ctx b)
-    OSL.Apply _ (OSL.Apply _ (OSL.MulN _) a) b ->
-      Term <$> (S11.Mul <$> translateToTerm ctx a
-                        <*> translateToTerm ctx b)
-    OSL.Apply _ (OSL.Apply _ (OSL.AddZ _) a) b ->
-      Term <$> (S11.Add <$> translateToTerm ctx a
-                        <*> translateToTerm ctx b)
-    OSL.Apply _ (OSL.Apply _ (OSL.MulZ _) a) b ->
-      Term <$> (S11.Mul <$> translateToTerm ctx a
-                        <*> translateToTerm ctx b)
+    OSL.Apply ann (OSL.Apply _ (OSL.AddN _) a) b ->
+      Term <$> (S11.Add <$> translateToTerm ctx (OSL.N ann) a
+                        <*> translateToTerm ctx (OSL.N ann) b)
+    OSL.Apply ann (OSL.Apply _ (OSL.MulN _) a) b ->
+      Term <$> (S11.Mul <$> translateToTerm ctx (OSL.N ann) a
+                        <*> translateToTerm ctx (OSL.N ann) b)
+    OSL.Apply ann (OSL.Apply _ (OSL.AddZ _) a) b ->
+      Term <$> (S11.Add <$> translateToTerm ctx (OSL.Z ann) a
+                        <*> translateToTerm ctx (OSL.Z ann) b)
+    OSL.Apply ann (OSL.Apply _ (OSL.MulZ _) a) b ->
+      Term <$> (S11.Mul <$> translateToTerm ctx (OSL.Z ann) a
+                        <*> translateToTerm ctx (OSL.Z ann) b)
     OSL.ConstN _ x -> return (Term (S11.Const x))
     OSL.ConstZ _ x -> return (Term (S11.Const x))
     OSL.ConstFin _ x -> return (Term (S11.Const x))
-    OSL.Apply _ (OSL.Apply _ (OSL.Pair _) a) b ->
-      Mapping <$>
-        (ProductMapping
-          <$> (LeftMapping <$> translateToMapping ctx a)
-          <*> (RightMapping <$> translateToMapping ctx b))
-    OSL.Apply _ (OSL.Pi1 _) a -> do
-      aM <- translateToMapping ctx a
+    OSL.Apply ann (OSL.Apply _ (OSL.Pair _) a) b ->
+      case termType of
+        OSL.Product _ aType bType -> do
+          Mapping <$>
+            (ProductMapping
+              <$> (LeftMapping <$> translateToMapping ctx aType a)
+              <*> (RightMapping <$> translateToMapping ctx bType b))
+        _ -> Left (ErrorMessage ann $
+          "expected a " <> pack (show termType))
+    OSL.Apply ann (OSL.Pi1 _) a -> do
+      aM <- translateToMapping ctx todo a
       case aM of
         ProductMapping (LeftMapping m) _ ->
           return (Mapping m)
-    OSL.Apply _ (OSL.Pi2 _) a -> do
-      aM <- translateToMapping ctx a
+        _ -> Left (ErrorMessage ann "expected a pair")
+    OSL.Apply ann (OSL.Pi2 _) a -> do
+      aM <- translateToMapping ctx todo a
       case aM of
         ProductMapping _ (RightMapping m) ->
           return (Mapping m)
+        _ -> Left (ErrorMessage ann "expected a pair")
+    OSL.Apply _ (OSL.Iota1 _) a ->
+      case termType of
+        OSL.Coproduct _ b c -> do
+          aM <- translateToMapping ctx b a
+          bM <- getArbitraryMapping ctx c
+          return . Mapping
+            $ CoproductMapping
+              (ChoiceMapping (S11.Const 0))
+              (LeftMapping aM)
+              (RightMapping bM)
+
+
+getArbitraryMapping
+  :: TranslationContext ann
+  -> OSL.Type ann
+  -> Either (ErrorMessage ann) (Mapping S11.Term)
+getArbitraryMapping = todo
 
 
 translateToMapping
   :: TranslationContext ann
+  -> OSL.Type ann
   -> OSL.Term ann
   -> Either (ErrorMessage ann) (Mapping S11.Term)
-translateToMapping c t =
-  case translate c t of
+translateToMapping c tType t =
+  case translate c tType t of
     Right (Mapping m) -> return m
     Right (Term t') -> return (ScalarMapping t')
     
 
 translateToTerm
   :: TranslationContext ann
+  -> OSL.Type ann
   -> OSL.Term ann
   -> Either (ErrorMessage ann) S11.Term
-translateToTerm c t =
-  case translate c t of
+translateToTerm c tType t =
+  case translate c tType t of
     Right (Term t') -> return t'
     Right (Mapping (ScalarMapping t')) -> return t'
     Right _ -> Left (ErrorMessage (termAnnotation t)
