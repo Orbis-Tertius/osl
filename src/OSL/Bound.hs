96e97579a8097a23ae50506e0e3aaaf2c87ab83f
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-08-26 15:41:14 -0400

    wip

---
+++ src/OSL/Bound.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE LambdaCase #-}
+
+
+module OSL.Bound (boundAnnotation) where
+
+
+import OSL.Types.OSL (Bound (..))
+
+
+boundAnnotation :: Bound ann -> ann
+boundAnnotation =
+  \case
+    ScalarBound ann _ -> ann
+    ProductBound ann _ _ -> ann
+    CoproductBound ann _ _ -> ann
+    FunctionBound ann _ _ -> ann
+    ListBound ann _ -> ann
+    MaybeBound ann _ -> ann
+    MapBound ann _ _ -> ann
+    ToBound ann _ _ -> ann
