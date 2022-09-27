b1355d72256951e185d6b5f9ee9757b5ad3297e7
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-09-27 18:46:39 -0400

    refactor

--- src/Halo2/Types/Circuit.hs
+++ src/Halo2/Types/Circuit.hs
@@ -6,17 +6,26 @@ module Halo2.Types.Circuit ( Circuit (Circuit) ) where
 
 
 import           Halo2.Prelude
-import           Halo2.Types.CircuitConfig       (CircuitConfig)
 import           Halo2.Types.EqualityConstraints (EqualityConstraints)
 import           Halo2.Types.FixedVariableValues (FixedVariableValues)
 import           Halo2.Types.RowCount            (RowCount)
-
+import           Halo2.Types.ColumnTypes                  (ColumnTypes)
+import           Halo2.Types.EqualityConstrainableColumns (EqualityConstrainableColumns)
+import           Halo2.Types.FiniteField                  (FiniteField)
+import           Halo2.Types.LookupArguments              (LookupArguments)
+import           Halo2.Types.PolynomialConstraints        (PolynomialConstraints)
+import           Halo2.Types.PolynomialDegreeBound        (PolynomialDegreeBound)
 
 data Circuit =
   Circuit
-  { config              :: CircuitConfig
-  , rowCount            :: RowCount
-  , equalityConstraints :: EqualityConstraints
-  , fixedVariableValues :: FixedVariableValues
+  { field                        :: FiniteField
+  , columnTypes                  :: ColumnTypes
+  , equalityConstrainableColumns :: EqualityConstrainableColumns
+  , polynomialDegreeBound        :: PolynomialDegreeBound
+  , polynomialConstraints        :: PolynomialConstraints
+  , lookupArguments              :: LookupArguments
+  , rowCount                     :: RowCount
+  , equalityConstraints          :: EqualityConstraints
+  , fixedVariableValues          :: FixedVariableValues
   }
   deriving (Eq, Ord, Show, Generic)
