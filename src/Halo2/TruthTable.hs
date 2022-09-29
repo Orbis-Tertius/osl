67e872fa8cd84f582639853181510cc5aa8e3acb
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-09-29 05:44:10 -0400

    truth table fixed columns

---
+++ src/Halo2/TruthTable.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+
+module Halo2.TruthTable
+  ( getByteRangeColumn
+  , getZeroIndicatorColumn
+  ) where
+
+
+import Cast (intToInteger)
+import Halo2.Prelude
+import Halo2.Types.BitsPerByte (BitsPerByte (..))
+import Halo2.Types.FieldElement (FieldElement (..))
+import Halo2.Types.FixedColumn (FixedColumn (..))
+import Halo2.Types.RowCount (RowCount (..))
+
+
+getByteRangeColumn :: BitsPerByte -> RowCount -> FixedColumn
+getByteRangeColumn (BitsPerByte b) (RowCount r) =
+  let m = (2 ^ b) - 1
+      m' = FieldElement (intToInteger m) in
+  FixedColumn ([0 .. m'] <> replicate (r - m) m')
+
+
+getZeroIndicatorColumn :: RowCount -> FixedColumn
+getZeroIndicatorColumn (RowCount n) =
+  FixedColumn (1 : replicate (n-1) 0)
