6ba970f66bd8282f2951e365a9f316f1522ac914
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-10-26 22:45:08 -0400

    logic constraint disjunctive normal forms

---
+++ src/Halo2/LogicConstraint.hs
@@ -0,0 +1,95 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+
+module Halo2.LogicConstraint
+  ( toDisjunctiveNormalForm
+  , toDisjunctiveNormalForms
+  ) where
+
+
+import Halo2.Types.LogicConstraint (LogicConstraint (Atom, Not, And, Or, Iff, Top, Bottom), AtomicLogicConstraint)
+import Halo2.Types.LogicConstraints (LogicConstraints (LogicConstraints))
+import Prelude hiding (and, or)
+
+
+newtype Disjunction = Disjunction [Conjunction]
+  deriving newtype (Semigroup, Monoid)
+
+
+newtype Conjunction = Conjunction [AlmostAtom]
+  deriving newtype (Semigroup, Monoid)
+
+
+data AlmostAtom = AlmostAtom Parity AtomicLogicConstraint
+
+
+data Parity = Positive | Negative
+
+
+toDisjunctiveNormalForms :: LogicConstraints -> LogicConstraints
+toDisjunctiveNormalForms (LogicConstraints gates bounds) =
+  LogicConstraints (toDisjunctiveNormalForm <$> gates) bounds
+
+
+toDisjunctiveNormalForm :: LogicConstraint -> LogicConstraint
+toDisjunctiveNormalForm = fromDisjunction . toDisjunction
+
+
+fromDisjunction :: Disjunction -> LogicConstraint
+fromDisjunction (Disjunction disjuncts) =
+  foldl Or Bottom
+  [ foldl And Top
+    [ case parity of
+        Positive -> Atom a
+        Negative -> Not (Atom a)
+    | AlmostAtom parity a <- conjuncts
+    ]
+  | Conjunction conjuncts <- disjuncts
+  ]
+
+
+atom :: AtomicLogicConstraint -> Disjunction
+atom = Disjunction . (:[]) . Conjunction . (:[]) . AlmostAtom Positive
+
+
+negAtom :: AtomicLogicConstraint -> Disjunction
+negAtom = Disjunction . (:[]) . Conjunction . (:[]) . AlmostAtom Negative
+
+
+and :: Disjunction -> Disjunction -> Disjunction
+and (Disjunction ps) (Disjunction qs) =
+  Disjunction
+    [ p <> q
+    | p <- ps
+    , q <- qs
+    ]
+
+
+top :: Disjunction
+top = Disjunction [mempty]
+
+
+bottom :: Disjunction
+bottom = mempty
+
+
+toDisjunction :: LogicConstraint -> Disjunction
+toDisjunction =
+  \case
+    Atom a -> atom a
+    Not (Atom a) -> negAtom a
+    And p q -> rec p `and` rec q
+    Not (And p q) -> rec (Not p) <> rec (Not q)
+    Or p q -> rec p <> rec q
+    Not (Or p q) -> rec (Not p) `and` rec (Not q)
+    Iff p q -> rec (Or (And p q) (And (Not p) (Not q)))
+    Not (Iff p q) -> (rec p `and` rec (Not q)) <> (rec (Not p) `and` rec q)
+    Top -> top
+    Not Top -> bottom
+    Bottom -> bottom
+    Not Bottom -> top
+    Not (Not p) -> rec p
+  where
+    rec = toDisjunction
