1c1c6ed43fa7120680ed7f8109dc2cf615eff879
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-11-14 03:01:53 -0500

    wip

--- src/Trace/ToArithmeticAIR.hs
+++ src/Trace/ToArithmeticAIR.hs
@@ -1,11 +1,14 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedLabels #-}
 
 module Trace.ToArithmeticAIR (traceTypeToArithmeticAIR) where
 
+import qualified Data.Map as Map
 import Halo2.Prelude
 import Halo2.Types.AIR (AIR (AIR), ArithmeticAIR)
-import Halo2.Types.ColumnIndex (ColumnIndex)
-import Halo2.Types.ColumnTypes (ColumnTypes)
+import Halo2.Types.ColumnIndex (ColumnIndex (ColumnIndex))
+import Halo2.Types.ColumnType (ColumnType (Fixed))
+import Halo2.Types.ColumnTypes (ColumnTypes (ColumnTypes))
 import Halo2.Types.FixedValues (FixedValues)
 import Halo2.Types.PolynomialConstraints (PolynomialConstraints)
 import Trace.Types (TraceType, StepTypeId, InputSubexpressionId, OutputSubexpressionId)
@@ -21,26 +24,44 @@ import Trace.Types (TraceType, StepTypeId, InputSubexpressionId, OutputSubexpres
 traceTypeToArithmeticAIR :: TraceType -> ArithmeticAIR
 traceTypeToArithmeticAIR t =
   AIR
-  (columnTypes t)
+  (columnTypes t m)
   (gateConstraints t)
   (t ^. #rowCount)
-  (additionalFixedValues t (fixedValueMappings t))
+  (additionalFixedValues t m)
+  where
+    m = fixedValueMappings t
 
-columnTypes :: TraceType -> ColumnTypes
-columnTypes = todo
+columnTypes :: TraceType -> FixedValueMappings -> ColumnTypes
+columnTypes t m =
+  (t ^. #columnTypes)
+  <> ColumnTypes
+     (Map.fromList
+       (zip
+         (ColumnIndex <$>
+           [length . Map.keys 
+             $ t ^. #columnTypes . #getColumnTypes])
+         (replicate (4 + length (m ^. #inputs)) Fixed)))
 
 gateConstraints :: TraceType -> PolynomialConstraints
 gateConstraints = todo
 
 newtype Mapping a =
   Mapping { unMapping :: ColumnIndex }
+  deriving Generic
+
+data CaseNumber
+
+data One
 
 data FixedValueMappings =
   FixedValueMappings
   { stepType :: Mapping StepTypeId
   , inputs :: [Mapping InputSubexpressionId]
   , output :: Mapping OutputSubexpressionId
+  , caseNumber :: Mapping CaseNumber
+  , one :: Mapping One
   }
+  deriving Generic
 
 fixedValueMappings :: TraceType -> FixedValueMappings
 fixedValueMappings = todo
