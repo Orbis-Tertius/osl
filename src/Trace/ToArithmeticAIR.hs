2b3236ff87e90bc65b8afe9bef458d341b8b0707
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-11-14 02:32:22 -0500

    wip

--- src/Trace/ToArithmeticAIR.hs
+++ src/Trace/ToArithmeticAIR.hs
@@ -3,6 +3,14 @@ module Trace.ToArithmeticAIR (traceTypeToArithmeticAIR) where
 import Halo2.Types.AIR (ArithmeticAIR)
 import Trace.Types (TraceType)
 
+-- Trace type arithmetic AIRs have the columnar structure
+-- of the trace type, with additional fixed columns for:
+--  * the table of links, and
+--  * the table {(i,1) | i < numCases}.
+--
+-- Trace type arithmetic AIR gate constraints entail that
+-- for each step of each case, the gate constraints of
+-- its step type are satisfied.
 traceTypeToArithmeticAIR :: TraceType -> ArithmeticAIR
 traceTypeToArithmeticAIR = todo
 
