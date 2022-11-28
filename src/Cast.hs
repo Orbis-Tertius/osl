ec3c4f1c6802144cbd7c62945beb0c14398e1584
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-11-28 08:38:25 -0500

    remove dependencies on haskell-fri-proofs and horizon-orbis

--- src/Cast.hs
+++ src/Cast.hs
@@ -5,12 +5,15 @@ module Cast
     word64ToInteger,
     integerToInt,
     integerToWord64,
+    integerToRatio,
+    word8ToInteger,
+    word64ToRatio
   )
 where
 
 import Data.Bits (toIntegralSized)
 import Data.Maybe (fromMaybe)
-import Data.Word (Word64)
+import Data.Word (Word64, Word8)
 import Die (die)
 
 intToInteger :: Int -> Integer
@@ -24,3 +27,12 @@ integerToInt = toIntegralSized
 
 integerToWord64 :: Integer -> Maybe Word64
 integerToWord64 = toIntegralSized
+
+integerToRatio :: Integer -> Rational
+integerToRatio = toRational
+
+word8ToInteger :: Word8 -> Integer
+word8ToInteger = fromMaybe (die "word8ToInteger partiality") . toIntegralSized
+
+word64ToRatio :: Word64 -> Rational
+word64ToRatio = integerToRatio . fromMaybe (die "word64ToRatio partiality") . toIntegralSized
