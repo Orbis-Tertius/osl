6b1e8cfda5121a54f63689b1676570b6d9b56785
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-11-28 11:07:48 -0500

    HasPolynomialVariables instances

--- src/Halo2/Circuit.hs
+++ src/Halo2/Circuit.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedLabels #-}
 
 module Halo2.Circuit
@@ -7,22 +8,65 @@ module Halo2.Circuit
   ) where
 
 
+import qualified Data.Map as Map
 import Halo2.Prelude
 import Halo2.Types.Circuit (Circuit)
+import Halo2.Types.InputExpression (InputExpression (getInputExpression))
+import Halo2.Types.LogicConstraint (LogicConstraint (Atom, Not, And, Or, Iff, Top, Bottom), AtomicLogicConstraint (Equals, LessThan))
 import Halo2.Types.LogicConstraints (LogicConstraints)
-import Halo2.Types.LookupArguments (LookupArguments)
+import Halo2.Types.LookupArgument (LookupArgument)
+import Halo2.Types.LookupArguments (LookupArguments (getLookupArguments))
+import Halo2.Types.Polynomial (Polynomial)
 import Halo2.Types.PolynomialVariable (PolynomialVariable)
+import Halo2.Types.PowerProduct (PowerProduct (getPowerProduct))
 import Stark.Types.Scalar (Scalar)
 
 
 class HasPolynomialVariables a where
   getPolynomialVariables :: a -> Set PolynomialVariable
 
+instance HasPolynomialVariables PowerProduct where
+  getPolynomialVariables = Map.keysSet . getPowerProduct
+
+instance HasPolynomialVariables Polynomial where
+  getPolynomialVariables =
+    mconcat . fmap getPolynomialVariables . Map.keys . (^. #monos)
+
+instance HasPolynomialVariables AtomicLogicConstraint where
+  getPolynomialVariables =
+    \case
+      Equals x y -> getPolynomialVariables x <> getPolynomialVariables y
+      LessThan x y -> getPolynomialVariables x <> getPolynomialVariables y
+
+instance HasPolynomialVariables LogicConstraint where
+  getPolynomialVariables x =
+    case x of
+      Atom y -> getPolynomialVariables y
+      Not y -> rec y
+      And y z -> rec y <> rec z
+      Or y z -> rec y <> rec z
+      Iff y z -> rec y <> rec z
+      Top -> mempty
+      Bottom -> mempty
+    where
+      rec = getPolynomialVariables
+
 instance HasPolynomialVariables LogicConstraints where
-  getPolynomialVariables = todo
+  getPolynomialVariables =
+    mconcat . fmap getPolynomialVariables . (^. #constraints)
+
+instance HasPolynomialVariables InputExpression where
+  getPolynomialVariables = getPolynomialVariables . getInputExpression
+
+instance HasPolynomialVariables LookupArgument where
+  getPolynomialVariables x =
+    mconcat 
+    ( getPolynomialVariables (x ^. #gate)
+      : (getPolynomialVariables . fst <$> (x ^. #tableMap)) )
 
 instance HasPolynomialVariables LookupArguments where
-  getPolynomialVariables = todo
+  getPolynomialVariables =
+    mconcat . fmap getPolynomialVariables . getLookupArguments
 
 instance HasPolynomialVariables a => HasPolynomialVariables (Circuit a) where
   getPolynomialVariables x =
