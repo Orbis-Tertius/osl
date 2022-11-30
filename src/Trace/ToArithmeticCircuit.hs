396040b02e2c2b10d838c0aa8469879028c7edf9
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-11-30 14:14:03 -0500

    add precondition check

--- src/Trace/ToArithmeticCircuit.hs
+++ src/Trace/ToArithmeticCircuit.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Trace.ToArithmeticCircuit (traceTypeToArithmeticCircuit) where
 
 import Control.Lens ((<&>))
 import Data.List.Extra (foldl', mconcatMap)
 import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Die (die)
 import Halo2.AIR (toCircuit)
 import qualified Halo2.Polynomial as P
 import Halo2.Prelude
@@ -16,7 +19,7 @@ import Halo2.Types.LookupArguments (LookupArguments (LookupArguments))
 import Halo2.Types.LookupTableColumn (LookupTableColumn (LookupTableColumn))
 import Halo2.Types.Polynomial (Polynomial)
 import Trace.ToArithmeticAIR (Mappings, mappings, traceTypeToArithmeticAIR)
-import Trace.Types (StepType, StepTypeId, TraceType)
+import Trace.Types (StepType, StepTypeId, TraceType, SubexpressionId)
 
 traceTypeToArithmeticCircuit ::
   TraceType ->
@@ -73,29 +76,44 @@ inputChecks t m =
         let y = t ^. #outputColumnIndex . #unOutputColumnIndex
     ]
 
--- TODO: add precondition checks
 linkChecks ::
   TraceType ->
   Mappings ->
   LookupArguments
 linkChecks t m =
-  LookupArguments
+  LookupArguments $
     [ LookupArgument
         (stepIndicatorGate t)
         ( zip
-            (InputExpression <$> ([tau] <> alphas <> [beta]))
+            (InputExpression <$> ([currentCase, tau] <> alphas <> [beta]))
             (LookupTableColumn <$> links)
         )
+    ] <>
+    [ LookupArgument
+        (P.plus (P.times gamma (stepIndicatorGate t)) (subexpressionIdGate m eidOut))
+        (zip (InputExpression <$> [currentCase, P.constant $ eidIn ^. #unPreconditionSubexpressionId . #unSubexpressionId])
+             (LookupTableColumn <$> [caseNumber, subexpressionId]))
+    | link <- Set.toList (t ^. #links),
+      let eidOut = link ^. #output . #unOutputSubexpressionId,
+      eidIn <- link ^. #preconditions
     ]
   where
-    tau, beta :: Polynomial
+    tau, beta, gamma, currentCase :: Polynomial
     alphas :: [Polynomial]
     links :: [ColumnIndex]
+    caseNumber, subexpressionId :: ColumnIndex
+    caseNumber = t ^. #caseNumberColumnIndex . #unCaseNumberColumnIndex
+    subexpressionId = m ^. #advice . #output . #unMapping
+    currentCase = P.var' caseNumber
+    gamma = P.constant $
+      case Set.lookupMax (t ^. #subexpressions) of
+        Just eid -> (eid ^. #unSubexpressionId) + 1
+        Nothing -> die "linkChecks: no subexpression ids (this is a compiler bug)"
     tau = P.var' $ t ^. #stepTypeColumnIndex . #unStepTypeColumnIndex
     alphas = P.var' <$> ((m ^. #advice . #inputs) <&> (^. #unMapping))
-    beta = P.var' $ m ^. #advice . #output . #unMapping
+    beta = P.var' subexpressionId
     links =
-      [m ^. #fixed . #stepType . #unMapping]
+      [caseNumber, m ^. #fixed . #stepType . #unMapping]
         <> ((m ^. #fixed . #inputs) <&> (^. #unMapping))
         <> [m ^. #fixed . #output . #unMapping]
 
@@ -167,3 +185,11 @@ stepTypeGate t sId =
       | sId' <- Map.keys (t ^. #stepTypes),
         sId' /= sId
     ]
+
+subexpressionIdGate ::
+  Mappings ->
+  SubexpressionId ->
+  Polynomial
+subexpressionIdGate m eid =
+  P.var' (m ^. #advice . #output . #unMapping)
+    `P.minus` P.constant (eid ^. #unSubexpressionId)
