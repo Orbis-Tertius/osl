38a05eac2514ab317b55e59814e481af6b0ce9ac
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-11-30 16:08:24 -0500

    circuit metrics types

---
+++ src/Halo2/Types/CircuitMetrics.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+
+module Halo2.Types.CircuitMetrics
+  ( CircuitMetrics (CircuitMetrics),
+    RowCount (RowCount),
+    ColumnClass (Advice, Instance, Fixed, All, EqualityConstrainable),
+    ColumnCounts (ColumnCounts),
+    ColumnCount (ColumnCount),
+    PolynomialDegreeBound (PolynomialDegreeBound),
+    GateConstraintCount (GateConstraintCount)
+  ) where
+
+import Data.Kind (Type)
+import GHC.Generics (Generic)
+import Halo2.Types.PolynomialDegreeBound (PolynomialDegreeBound (PolynomialDegreeBound))
+import Halo2.Types.RowCount (RowCount (RowCount))
+import Trace.Types (NumberOfCases)
+
+data CircuitMetrics =
+  CircuitMetrics
+  { rowCount :: RowCount,
+    numCases :: NumberOfCases,
+    columnCounts :: ColumnCounts,
+    polyDegreeBound :: PolynomialDegreeBound,
+    gateConstraintCount :: GateConstraintCount,
+    lookupArgumentCount :: LookupArgumentCount,
+    lookupTableSize :: LookupTableSize
+  }
+  deriving stock (Generic, Show)
+
+data ColumnClass =
+    Advice
+  | Instance
+  | Fixed
+  | All
+  | EqualityConstrainable
+
+data ColumnCounts =
+  ColumnCounts
+  { advice :: ColumnCount Advice,
+    instances :: ColumnCount Instance,
+    fixed :: ColumnCount Fixed,
+    all :: ColumnCount All,
+    eqc :: ColumnCount EqualityConstrainable
+  }
+  deriving stock (Generic, Show)
+
+type ColumnCount :: ColumnClass -> Type
+newtype ColumnCount a = ColumnCount { unColumnCount :: Int }
+  deriving stock (Generic)
+  deriving newtype (Show)
+
+newtype GateConstraintCount = GateConstraintCount { unGateConstraintCount :: Int }
+  deriving stock (Generic)
+  deriving newtype (Show)
+
+newtype LookupArgumentCount = LookupArgumentCount { unLookupArgumentCount :: Int }
+  deriving stock (Generic)
+  deriving newtype (Show)
+
+newtype LookupTableSize = LookupTableSize { unLookupTableSize :: Int }
+  deriving stock (Generic)
+  deriving newtype (Show)
