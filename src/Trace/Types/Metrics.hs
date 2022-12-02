62988b3b1d94b40af520a979fddb5138664e1b82
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-12-01 21:22:35 -0500

    trace type metrics

--- src/Trace/Types/Metrics.hs
+++ src/Trace/Types/Metrics.hs
@@ -9,34 +9,34 @@ module Trace.Types.Metrics
     SubexpressionCount (SubexpressionCount),
     LinkCount (LinkCount),
     ResultCount (ResultCount),
-    InputColumnCount (InputColumnCount)
-  ) where
+    InputColumnCount (InputColumnCount),
+  )
+where
 
 import GHC.Generics (Generic)
 import Trace.Types (NumberOfCases (NumberOfCases))
 
-newtype StepTypeCount = StepTypeCount { unStepTypeCount :: Int }
+newtype StepTypeCount = StepTypeCount {unStepTypeCount :: Int}
   deriving stock (Generic)
   deriving newtype (Show)
 
-newtype SubexpressionCount = SubexpressionCount { unSubexpressionCount :: Int }
+newtype SubexpressionCount = SubexpressionCount {unSubexpressionCount :: Int}
   deriving stock (Generic)
   deriving newtype (Show)
 
-newtype LinkCount = LinkCount { unLinkCount :: Int }
+newtype LinkCount = LinkCount {unLinkCount :: Int}
   deriving stock (Generic)
   deriving newtype (Show)
 
-newtype ResultCount = ResultCount { unResultCount :: Int }
+newtype ResultCount = ResultCount {unResultCount :: Int}
   deriving stock (Generic)
   deriving newtype (Show)
 
-newtype InputColumnCount = InputColumnCount { unInputColumnCount :: Int }
+newtype InputColumnCount = InputColumnCount {unInputColumnCount :: Int}
   deriving stock (Generic)
   deriving newtype (Show)
 
-data TraceTypeMetrics =
-  TraceTypeMetrics
+data TraceTypeMetrics = TraceTypeMetrics
   { stepTypeCount :: StepTypeCount,
     subexpressionCount :: SubexpressionCount,
     linkCount :: LinkCount,
