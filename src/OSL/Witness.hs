e0e1c374e50aea7fc3f5e99c941ca2a2d00b4ad1
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-12-08 23:13:25 -0500

    wip preprocess witnesses

--- src/OSL/Witness.hs
+++ src/OSL/Witness.hs
@@ -1,5 +1,6 @@
 module OSL.Witness (preprocessWitness) where
 
+import Data.Map (Map)
 import OSL.Types.Argument (Witness)
 import OSL.Types.ErrorMessage (ErrorMessage)
 import OSL.Types.OSL (Term)
@@ -7,6 +8,17 @@ import OSL.Types.PreprocessedWitness (PreprocessedWitness)
 
 preprocessWitness :: Term ann -> Witness -> Either (ErrorMessage ann) (PreprocessedWitness ann)
 preprocessWitness = todo
+
+-- The telescope of a subterm is the sequence of its enclosing subterms, beginning with
+-- the overall term and ending with the subterm itself. Having the telescope
+-- of a subterm helps with traversing the overall formula and concurrently traversing
+-- the witness in order to find the piece of the witness that it is relevant
+-- to the given annotation and the evaluation context.
+newtype Telescope ann = Telescope [Term ann]
+
+-- Get the map of subformula annotations to their telescopes.
+getSubformulaTelescopes :: Ord ann => Term ann -> Map ann (Telescope ann)
+getSubformulaTelescopes = todo
 
 todo :: a
 todo = todo
