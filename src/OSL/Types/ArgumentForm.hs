7181eb3334c03837afcd82f199c1ae42cf83689b
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-12-08 21:37:10 -0500

    argument forms

--- src/OSL/Types/ArgumentForm.hs
+++ src/OSL/Types/ArgumentForm.hs
@@ -10,7 +10,7 @@ module OSL.Types.ArgumentForm
 where
 
 import GHC.Generics (Generic)
-import OSL.Types.OSL (Type)
+import OSL.Types.OSL (Type (Fin, Product))
 
 data ArgumentForm = ArgumentForm
   { statementType :: StatementType,
@@ -22,6 +22,24 @@ newtype StatementType = StatementType {unStatementType :: Type ()}
   deriving stock (Eq, Ord, Generic)
   deriving newtype (Show)
 
+instance Semigroup StatementType where
+  StatementType (Fin () 1) <> b = b
+  a <> StatementType (Fin () 1) = a
+  StatementType a <> StatementType b =
+    StatementType (Product () a b)
+
+instance Monoid StatementType where
+  mempty = StatementType (Fin () 1)
+
 newtype WitnessType = WitnessType {unWitnessType :: Type ()}
   deriving stock (Eq, Ord, Generic)
   deriving newtype (Show)
+
+instance Semigroup WitnessType where
+  WitnessType (Fin () 1) <> b = b
+  a <> WitnessType (Fin () 1) = a
+  WitnessType a <> WitnessType b =
+    WitnessType (Product () a b)
+
+instance Monoid WitnessType where
+  mempty = WitnessType (Fin () 1)
