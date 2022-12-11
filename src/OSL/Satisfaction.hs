f7aad5d7b2504b64294a3228a3942b51e1787dd0
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-12-11 14:10:47 -0500

    satisfaction?

--- src/OSL/Satisfaction.hs
+++ src/OSL/Satisfaction.hs
@@ -4,21 +4,29 @@
 module OSL.Satisfaction (satisfies) where
 
 import qualified Data.Map as Map
+import Data.Tuple.Extra (curry3)
+import OSL.Evaluation (evaluate)
 import OSL.Types.Argument (Argument (Argument), Statement (Statement))
-import OSL.Types.EvaluationContext (EvaluationContext (EvaluationContext))
 import OSL.Types.ErrorMessage (ErrorMessage)
-import OSL.Types.OSL (ContextType (Global), Term (Lambda), ValidContext)
-import OSL.Types.Value (Value (Pair'))
+import OSL.Types.EvaluationContext (EvaluationContext (EvaluationContext))
+import OSL.Types.OSL (ContextType (Global, Local), Declaration (FreeVariable), Term (Lambda), Type (F), ValidContext (ValidContext))
+import OSL.Types.Value (Value (Bool, Pair'))
+import OSL.Witness (preprocessWitness)
 
-satisfies :: ValidContext 'Global ann -> EvaluationContext -> Term ann -> Argument -> Either (ErrorMessage ann) Bool
-satisfies c e = curry $
+satisfies :: (Ord ann, Show ann) => ValidContext 'Global ann -> ValidContext 'Local ann -> EvaluationContext -> Type ann -> Term ann -> Argument -> Either (ErrorMessage ann) Bool
+satisfies gc lc e = curry3 $
   \case
-    (Lambda _ann v _b body, Argument (Statement (Pair' vs s')) w) ->
-      satisfies
-        c
-        (e <> EvaluationContext (Map.singleton v vs))
-        body
-        (Argument (Statement s') w)
-
-todo :: a
-todo = todo
+    ( F _ann _n a b,
+      Lambda _ann' v _a' body,
+      Argument (Statement (Pair' vs s')) w
+      ) ->
+        satisfies
+          gc
+          (lc <> ValidContext (Map.singleton v (FreeVariable a)))
+          (e <> EvaluationContext (Map.singleton v vs))
+          b
+          body
+          (Argument (Statement s') w)
+    (a, x, Argument _ w) -> do
+      pw <- preprocessWitness x w
+      (== Bool True) <$> evaluate gc pw lc a x e
