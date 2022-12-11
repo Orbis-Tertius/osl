69af2936f338b3318960e2b4f2354480411e27c8
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-12-11 16:39:04 -0500

    wip simplifyValue

--- src/OSL/SimplifyType.hs
+++ src/OSL/SimplifyType.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module OSL.SimplifyType
   ( simplifyType,
@@ -6,9 +7,11 @@ module OSL.SimplifyType
     complexifyValue
   ) where
 
+import qualified Data.Map as Map
 import Data.Maybe (fromMaybe)
+import OSL.Types.ErrorMessage (ErrorMessage (ErrorMessage))
 import OSL.Types.OSL (Type (Prop, F, P, N, Z, Fp, Fin, Product, Coproduct, NamedType, Maybe, List, Map))
-import OSL.Types.Value (Value)
+import OSL.Types.Value (Value (Bool, Fun, Fin', Nat, Int, Fp'))
 
 -- This operation cannot fail. A result of Nothing
 -- signifies that the type simplifies to the unit type
@@ -57,8 +60,41 @@ simplifyType =
 
     rec' ann = fromMaybe (Fin ann 1) . rec
 
-simplifyValue :: Type ann -> Value -> Value
-simplifyValue = todo
+simplifyValue :: Type ann -> Value -> Either (ErrorMessage ann) Value
+simplifyValue =
+  curry $
+    \case
+      (Prop _, Bool v) -> pure (Bool v)
+      (F ann _n a b, Fun f) ->
+        case simplifyType b of
+          Nothing -> pure (Fin' 0)
+          Just _ ->
+            case simplifyType a of
+              Nothing ->
+                case Map.lookup (Fin' 0) f of
+                  Just v -> simplifyValue b v
+                  Nothing -> Left . ErrorMessage ann $
+                    "function not defined on value fin(0)"
+              Just _ ->
+                Fun . Map.fromList <$> sequence
+                  [ (,) <$> simplifyValue a x <*> simplifyValue b y
+                  | (x,y) <- Map.toList f
+                  ]
+      (P ann _n a b, Fun f) ->
+        case simplifyType b of
+          Nothing -> pure (Fin' 0)
+          Just _ ->
+            case simplifyType a of
+              Nothing -> pure (Fin' 0)
+              Just _ ->
+                Fun . Map.fromList <$> sequence
+                  [ (,) <$> simplifyValue a x <*> simplifyValue b y
+                  | (x,y) <- Map.toList f
+                  ]
+      (N ann, Nat x) -> pure (Nat x)
+      (Z ann, Int x) -> pure (Int x)
+      (Fp ann, Fp' x) -> pure (Fp' x)
+      (Fin ann _, Fin' x) -> pure (Fin' x)
 
 complexifyValue :: Type ann -> Value -> Value
 complexifyValue = todo
