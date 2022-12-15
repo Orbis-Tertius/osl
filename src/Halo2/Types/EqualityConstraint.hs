6d6fc039f23e13b577b79bef22a03131361bd9de
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-12-14 19:26:34 -0600

    remove old todo

--- src/Halo2/Types/EqualityConstraint.hs
+++ src/Halo2/Types/EqualityConstraint.hs
@@ -8,8 +8,6 @@ import Data.Set (toList)
 import Halo2.Prelude
 import Halo2.Types.CellReference
 
--- TODO: distinguish absolute and relative cell references
--- at type level
 newtype EqualityConstraint = EqualityConstraint
   {getEqualityConstraint :: Set CellReference}
   deriving (Eq, Ord, Generic)
