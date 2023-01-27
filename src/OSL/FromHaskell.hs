c51ee4aadf61aa25ba0c02d75b2884c0f98699d1
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2023-01-27 08:09:53 -0600

    wip generics

--- src/OSL/FromHaskell.hs
+++ src/OSL/FromHaskell.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
@@ -14,7 +16,8 @@ module OSL.FromHaskell
 
 import Data.Kind (Type)
 import Data.Proxy (Proxy (Proxy))
-import GHC.Generics (Rep, U1, (:*:))
+import GHC.Exts (Symbol)
+import GHC.Generics (Rep, U1, (:*:), (:+:), M1, D, C, S, K1, R, Meta (MetaData))
 import qualified OSL.Types.OSL as OSL
 
 class ToOSLType a where
@@ -26,12 +29,56 @@ class GToOSLType (a :: Type -> Type) where
 instance GToOSLType U1 where
   gtoOSLType _ _ = OSL.Fin () 1
 
+instance GToOSLType a => GToOSLType (M1 D m a) where
+  gtoOSLType (Proxy :: Proxy (M1 D m a)) c =
+    gtoOSLType (Proxy :: Proxy a) c
+
+instance GToOSLType a => GToOSLType (M1 C m a) where
+  gtoOSLType (Proxy :: Proxy (M1 C m a)) c =
+    gtoOSLType (Proxy :: Proxy a) c
+
+instance GToOSLType a => GToOSLType (M1 S m a) where
+  gtoOSLType (Proxy :: Proxy (M1 S m a)) c =
+    gtoOSLType (Proxy :: Proxy a) c
+
+instance ScalarToOSLType a ra sa ma pa nt
+           => GToOSLType (M1 S (MetaData sa ma pa nt) ra) where
+  gtoOSLType (Proxy :: Proxy (M1 S (MetaData sa ma pa nt))) c =
+    scalarToOSLType (Proxy :: Proxy a) c
+
+instance GToOSLType (Rep a) => GToOSLType (K1 R a) where
+  gtoOSLType (Proxy :: Proxy (K1 R a)) c =
+    gtoOSLType (Proxy :: Proxy (Rep a)) c
+
 instance ( GToOSLType a, GToOSLType b )
            => GToOSLType (a :*: b) where
   gtoOSLType (Proxy :: Proxy (a :*: b)) c =
     OSL.Product ()
       (gtoOSLType (Proxy :: Proxy a) c)
       (gtoOSLType (Proxy :: Proxy b) c)
+
+instance ( GToOSLType a, GToOSLType b )
+           => GToOSLType (a :+: b) where
+  gtoOSLType (Proxy :: Proxy (a :+: b)) c =
+    OSL.Coproduct ()
+      (gtoOSLType (Proxy :: Proxy a) c)
+      (gtoOSLType (Proxy :: Proxy b) c)
+
+-- To handle scalar types, maybe make a new typeclass for finding
+-- their OSL reps?
+
+class ScalarToOSLType
+        (a :: Type)
+        (ra :: Type -> Type)
+        (sa :: Symbol)
+        (ma :: Symbol)
+        (pa :: Symbol)
+        (nt :: Bool)
+        | a -> sa, a -> ra, a -> ma, a -> pa, a -> nt where
+  scalarToOSLType ::
+    Proxy a ->
+    OSL.ValidContext 'OSL.Global ann ->
+    OSL.Type ()
 
 instance GToOSLType (Rep a) => ToOSLType a where
   toOSLType (Proxy :: Proxy a) = gtoOSLType (Proxy @(Rep a))
